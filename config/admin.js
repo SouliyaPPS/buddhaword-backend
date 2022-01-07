3a6670bf4f03f47911c840c608052ff2837ab310
Souliya PPS <souliyappsdev@gmail.com>
2022-01-07 21:32:49 +0700

    backend

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '0175963588216d9b214b95727ce40844'),
+    secret: env('ADMIN_JWT_SECRET', '1e3a203094c2ef4689284ae5f156f468'),
   },
 });
